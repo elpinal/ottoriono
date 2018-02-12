0ad3edf632167915ec1a5752d7e90a33e2a72f04
elpinal <elpinal@users.noreply.github.com>
2018-02-12 23:38:07 +0900

    Add Expr and Term

---
+++ src/expr.rs
@@ -0,0 +1,11 @@
+pub enum Expr {
+    Add(Box<Expr>, Term),
+    Sub(Box<Expr>, Term),
+    Term(Term),
+}
+
+pub enum Term {
+    Var(usize, usize),
+    Abs(String, Box<Term>),
+    App(Box<Term>, Box<Term>),
+}
