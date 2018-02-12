faae88f368d07fcdd67620a0a27e024a75a1b1f5
elpinal <elpinal@users.noreply.github.com>
2018-02-12 23:55:20 +0900

    Use std::io::Bytes

--- src/parse.rs
+++ src/parse.rs
@@ -9,11 +9,11 @@ fn parse<R>(mut r: R) -> Result<expr::Expr, Error>
 where
     R: Read,
 {
-    let mut buf = [0; 10];
-    r.read(&mut buf);
-    match buf[0] {
+    let mut buf = r.bytes();
+    let b = buf.next();
+    match b {
         b'0'...b'9' => {
-            return Ok(expr::Expr::Term(expr::Term::Int((buf[0] - b'0') as isize)));
+            return Ok(expr::Expr::Term(expr::Term::Int((b - b'0') as isize)));
         }
         _ => unimplemented!(),
     }
