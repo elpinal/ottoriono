94095c65fccfb3d92b2d978df43099656bd09189
elpinal <elpinal@users.noreply.github.com>
2018-02-13 10:45:16 +0900

    Write Wrapper::next

--- src/parse.rs
+++ src/parse.rs
@@ -1,17 +1,19 @@
 use expr;
 
-use std::io::Read;
+use std::io;
+use std::io::{Bytes, Read};
 
 enum Error {
     EOF,
+    Io(io::Error),
 }
 
-fn parse<R>(mut r: R) -> Result<expr::Expr, Error>
+fn parse<R>(r: R) -> Result<expr::Expr, Error>
 where
     R: Read,
 {
-    let mut buf = r.bytes();
-    let b = buf.next();
+    let mut buf = Wrapper { b: r.bytes() };
+    let b = buf.next()?;
     match b {
         b'0'...b'9' => {
             return Ok(expr::Expr::Term(expr::Term::Int((b - b'0') as isize)));
@@ -20,13 +22,15 @@ where
     }
 }
 
-struct Wrapper {
+struct Wrapper<R> {
+    b: Bytes<R>,
 }
 
-impl Wrapper {
-    fn next(b: B) -> Option<()>
-    where
-        B: Bytes,
-    {
+impl<R: Read> Wrapper<R> {
+    fn next(&mut self) -> Result<u8, Error> {
+        match self.b.next() {
+            Some(r) => r.map_err(|e| Error::Io(e)),
+            None => Err(Error::EOF),
+        }
     }
 }
