31281a1381e1cc5c4c7fe6f2fd20ac08611b4555
elpinal <elpinal@users.noreply.github.com>
2018-02-19 21:47:47 +0900

    Make Position an instance of Display

--- src/parse.rs
+++ src/parse.rs
@@ -386,6 +386,12 @@ impl fmt::Display for Error {
     }
 }
 
+impl fmt::Display for Position {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}:{}", self.0, self.1)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
