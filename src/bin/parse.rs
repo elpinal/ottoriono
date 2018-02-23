917c5048c69e3e70eebe8e243c79c3ed26effe84
elpinal <elpinal@users.noreply.github.com>
2018-02-23 21:38:56 +0900

    Enable to parse user's input repeatedly

--- src/bin/parse.rs
+++ src/bin/parse.rs
@@ -1,14 +1,24 @@
 extern crate ottoriono;
 
 use ottoriono::parse;
+use ottoriono::parse::Error;
 
 use std::io;
+use std::process::exit;
 
 fn main() {
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+        exit(1);
+    }
+}
+
+fn run() -> Result<(), Error> {
     let stdin = io::stdin();
-    let handle = stdin.lock();
-    match parse::parse(handle) {
-        Ok(e) => println!("{}", e),
-        Err(e) => eprintln!("{}", e),
+    let mut buf = String::new();
+    loop {
+        stdin.read_line(&mut buf)?;
+        println!("{}", parse::parse(buf.as_bytes())?);
+        buf.truncate(0);
     }
 }
